115a39b88229394f072b814cfe076c2682e6c98b
mezez <ekemammezez@gmail.com>
2021-11-20 21:30:38 +0100

    updates

--- using-xml-and-json-docs-with-php/using-xml-and-json.php
+++ using-xml-and-json-docs-with-php/using-xml-and-json.php
@@ -12,14 +12,24 @@ function useXmlAndJson(){
         ->setImmutable(false)
     );
 
-    $generator->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/schema'), __DIR__ . '/result');
+    try {
+        $generator->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/schema'), __DIR__ . '/result');
+    } catch (\PHPModelGenerator\Exception\FileSystemException $e) {
+        echo json_encode($e->getMessage());
+    } catch (\PHPModelGenerator\Exception\RenderException $e) {
+        echo json_encode($e->getMessage());
+    } catch (\PHPModelGenerator\Exception\SchemaException $e) {
+        echo json_encode($e->getMessage());
+    }
 }
 
 
-echo "<p>Bootstrapping the API implementation in PHP: Classes for your datamodels<br>
-From the WeatherForecast datamodel, generate the set of classes using the php Wol-soft plugin</p><br>";
+echo "<p><b>Bootstrapping the API implementation in PHP: Classes for your datamodels</b><hr>
+From the WeatherForecast datamodel, generate the set of classes using the php Wol-soft plugin</p>";
 echo "<p>Please Note: Te wol-soft plugin does not support the date-time format for date in the schema file, so that line has been removed</p>";
 echo "<p>See interop/using-xml-and-json-docs-with-php/result folder for generated class</p>";
+echo "<hr>";
+echo "<p>If you get the message '[path] doesn't exist or is not empty', please empty the contents of the interop/using-xml-and-json-docs-with-php/result folder</p>";
 echo "<hr>";
 
 useXmlAndJson();
